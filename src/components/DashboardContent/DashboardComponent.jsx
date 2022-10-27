df8e6d154b62a4da8560eceb61eca07841e803cb
gift56 <efeasiughu@gmail.com>
2022-10-27 15:58:08 +0100

    added another bos

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -4,12 +4,21 @@ import { BsBarChartLine } from "react-icons/bs";
 const DashboardComponent = () => {
   return (
     <main className="p-5 grow bg-white my-2 mr-2 rounded-lg">
-      <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
-        <BsBarChartLine className="w-8 h-8" />
-        <div className="">
-            <h2 className="text-2xl font-bold leading-4">$345</h2>
-            <h3 className="">Profit today</h3>
-        </div>
+      <div>
+          <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
+            <BsBarChartLine className="w-8 h-8" />
+            <div className="">
+                <h2 className="text-2xl font-bold leading-4">$345</h2>
+                <h3 className="">Profit today</h3>
+            </div>
+          </div>
+          <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
+            <BsBarChartLine className="w-8 h-8" />
+            <div className="">
+                <h2 className="text-2xl font-bold leading-4">$345</h2>
+                <h3 className="">Profit today</h3>
+            </div>
+          </div>
       </div>
     </main>
   );
