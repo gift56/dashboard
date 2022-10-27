ecc18b14c3ab29940700bc607f94a3f2fa152b14
gift56 <efeasiughu@gmail.com>
2022-10-27 15:23:19 +0100

    added some padding

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,35 @@
 import {
-    AiOutlineHome,
-    AiOutlineUnorderedList,
-    AiOutlineInbox,
-    AiOutlineStar,
-  } from "react-icons/ai";
-  import { BsGear } from "react-icons/bs";
+  AiOutlineHome,
+  AiOutlineUnorderedList,
+  AiOutlineInbox,
+  AiOutlineStar,
+} from "react-icons/ai";
+import { BsGear } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
     <aside className="bg-primary text-white p-5">
       <nav>
-        <a href="#dashboard" className="p-2 flex gap-2 items-center">
+        <a
+          href="#dashboard"
+          className="bg-white text-primary rounded-xl px-4 py-2 flex gap-2 items-center"
+        >
           <AiOutlineHome />
           <span>Dashboard</span>
         </a>
-        <a href="#order" className=" p-2 flex gap-2 items-center">
+        <a href="#order" className="px-4 py-2 flex gap-2 items-center">
           <AiOutlineUnorderedList />
           <span>Orders</span>
         </a>
-        <a href="#products" className="flex gap-2 items-center p-2 ">
+        <a href="#products" className="flex gap-2 items-center px-4 py-2">
           <AiOutlineInbox />
           Products
         </a>
-        <a href="#review" className="flex gap-2 items-center p-2 ">
+        <a href="#review" className="flex gap-2 items-center px-4 py-2">
           <AiOutlineStar />
           <span>Reviews</span>
         </a>
-        <a href="#settings" className="flex gap-2 items-center p-2 ">
+        <a href="#settings" className="flex gap-2 items-center px-4 py-2">
           <BsGear />
           <span>Settings</span>
         </a>
