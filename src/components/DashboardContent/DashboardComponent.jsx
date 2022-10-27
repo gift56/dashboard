a0361ecc287ef25709b90e70623559eb495feadc
gift56 <efeasiughu@gmail.com>
2022-10-27 15:42:10 +0100

    added some margin

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -3,8 +3,8 @@ import { BsBarChartLine } from "react-icons/bs";
 
 const DashboardComponent = () => {
   return (
-    <main className="">
-      <div className="bg-blue-300 text-primary p-5">
+    <main className="p-5 ">
+      <div className="bg-blue-300 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md">
         <BsBarChartLine />
         <h2>$345</h2>
         <h3>Profit today</h3>
