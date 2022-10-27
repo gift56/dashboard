a172193b1cffe38f1c3321904704e6bcc8e8b17e
gift56 <efeasiughu@gmail.com>
2022-10-27 15:17:50 +0100

    imported some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
 import React from "react";
-import {
-  AiOutlineHome,
-  AiOutlineUnorderedList,
-  AiOutlineInbox,
-  AiOutlineStar,
-} from "react-icons/ai";
-import { BsGear } from "react-icons/bs";
 
 const App = () => {
   return (
     <div className="flex gap-3 min-h-screen">
-      <aside className="bg-primary text-white p-5">
-        <nav>
-          <a href="#dashboard" className="p-2 flex gap-2 items-center">
-            <AiOutlineHome />
-            <span>Dashboard</span>
-          </a>
-          <a href="#order" className=" p-2 flex gap-2 items-center">
-            <AiOutlineUnorderedList />
-            <span>Orders</span>
-          </a>
-          <a href="#products" className="flex gap-2 items-center p-2 ">
-            <AiOutlineInbox />
-            Products
-          </a>
-          <a href="#review" className="flex gap-2 items-center p-2 ">
-            <AiOutlineStar />
-            <span>Reviews</span>
-          </a>
-          <a href="#settings" className="flex gap-2 items-center p-2 ">
-            <BsGear />
-            <span>Settings</span>
-          </a>
-        </nav>
-      </aside>
+      <Sidebar />
       <main>Main</main>
     </div>
   );
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,38 @@
+import {
+    AiOutlineHome,
+    AiOutlineUnorderedList,
+    AiOutlineInbox,
+    AiOutlineStar,
+  } from "react-icons/ai";
+  import { BsGear } from "react-icons/bs";
+
+const Sidebar = () => {
+  return (
+    <aside className="bg-primary text-white p-5">
+      <nav>
+        <a href="#dashboard" className="p-2 flex gap-2 items-center">
+          <AiOutlineHome />
+          <span>Dashboard</span>
+        </a>
+        <a href="#order" className=" p-2 flex gap-2 items-center">
+          <AiOutlineUnorderedList />
+          <span>Orders</span>
+        </a>
+        <a href="#products" className="flex gap-2 items-center p-2 ">
+          <AiOutlineInbox />
+          Products
+        </a>
+        <a href="#review" className="flex gap-2 items-center p-2 ">
+          <AiOutlineStar />
+          <span>Reviews</span>
+        </a>
+        <a href="#settings" className="flex gap-2 items-center p-2 ">
+          <BsGear />
+          <span>Settings</span>
+        </a>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
