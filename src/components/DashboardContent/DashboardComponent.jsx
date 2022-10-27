eac2ff4a548afcd2762cd7e75c5476f42009ccaa
gift56 <efeasiughu@gmail.com>
2022-10-27 15:56:33 +0100

    added required stuff

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -4,10 +4,12 @@ import { BsBarChartLine } from "react-icons/bs";
 const DashboardComponent = () => {
   return (
     <main className="p-5 grow bg-white my-2 mr-2 rounded-lg">
-      <div className="bg-blue-300 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md">
-        <BsBarChartLine />
-        <h2>$345</h2>
-        <h3>Profit today</h3>
+      <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex justify-between items-center">
+        <BsBarChartLine className="w-8 h-8" />
+        <div className="">
+            <h2>$345</h2>
+            <h3>Profit today</h3>
+        </div>
       </div>
     </main>
   );
