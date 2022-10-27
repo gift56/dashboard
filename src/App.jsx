608c6f9e9cff333ba2802be83b79b32ef032233d
gift56 <efeasiughu@gmail.com>
2022-10-27 13:55:15 +0100

    added my varible

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const App = () => {
   return (
     <div className="flex gap-3">
-      <aside className="">SideBar</aside>
+      <aside className="bg-primary text-white p-5">
+        <nav>
+          <a href="">Dashboard</a>
+          <a href="">Orders</a>
+          <a href="">Products</a>
+          <a href="">Reviews</a>
+          <a href=""></a>
+        </nav>
+      </aside>
       <main>Main</main>
     </div>
   );
