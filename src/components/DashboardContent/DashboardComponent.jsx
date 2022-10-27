18da7e380db18151f9dea15c4cdc836b64704802
gift56 <efeasiughu@gmail.com>
2022-10-27 15:38:50 +0100

    changed icons

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -4,7 +4,7 @@ import { BsBarChartLine } from "react-icons/bs";
 const DashboardComponent = () => {
   return (
     <main>
-      <div className="bg-blue-300 text-primary">
+      <div className="bg-blue-300 text-primary p-5">
         <BsBarChartLine />
         <h2>$345</h2>
         <h3>Profit today</h3>
