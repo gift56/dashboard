9284a5b19675b9e89afdbfe18f5e375ad5d8d37d
gift56 <efeasiughu@gmail.com>
2022-10-27 15:51:53 +0100

    reduce some stuff

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -3,7 +3,7 @@ import { BsBarChartLine } from "react-icons/bs";
 
 const DashboardComponent = () => {
   return (
-    <main className="p-5  grow  bg-white my-4 mr-4">
+    <main className="p-5 grow bg-white my-2 mr-2">
       <div className="bg-blue-300 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md">
         <BsBarChartLine />
         <h2>$345</h2>
