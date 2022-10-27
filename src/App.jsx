8c07e9fe18b73af4be524474aeef8994c6631b96
gift56 <efeasiughu@gmail.com>
2022-10-27 13:50:34 +0100

    added some classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <aside>SideBar</aside>
+    <div className="flex gap-3">
+      <aside className="">SideBar</aside>
       <main>Main</main>
     </div>
   );
