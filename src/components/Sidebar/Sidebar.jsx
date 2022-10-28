860ca74820cb6087061cd97ca44d437a6b2a858a
gift56 <efeasiughu@gmail.com>
2022-10-28 13:06:05 +0100

    added some classes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,23 +5,23 @@ import {
   AiOutlineStar,
 } from "react-icons/ai";
 import { BsGear, BsShop } from "react-icons/bs";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <aside className="bg-primary text-white p-5 py-10 pr-0">
-      <a href="" className="flex mb-6 items-center justify-center mr-2 gap-1">
+      <Link to="/" className="flex mb-6 items-center justify-center mr-2 gap-1">
         <BsShop />
         <span>YourShop</span>
-      </a>
+      </Link>
       <nav>
-        <a
-          href="#dashboard"
+        <Link
+          to="/"
           className="bg-white text-primary rounded-xl rounded-r-none px-4 py-2 pr-7 flex gap-2 items-center"
         >
           <AiOutlineHome />
           <span>Dashboard</span>
-        </a>
+        </Link>
         <a href="#order" className="px-4 py-2 flex gap-2 items-center">
           <AiOutlineUnorderedList />
           <span>Orders</span>
