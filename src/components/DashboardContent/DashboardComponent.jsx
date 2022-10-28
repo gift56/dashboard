9f30d3dde90a734d4b224968f9c10406db802fdc
gift56 <efeasiughu@gmail.com>
2022-10-28 11:48:46 +0100

    maded it smaller

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -14,7 +14,7 @@ const DashboardComponent = () => {
           <h2 className="text-primary font-bold">Top selling products</h2>
           <div>
             <div className="flex items-center gap-3">
-              <span>1</span>
+              <span className="text-gray-500 text-sm">1.</span>
               <img
                 src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
                 alt="iamges"
