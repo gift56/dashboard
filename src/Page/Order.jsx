305d5dcd167f8dc35a93ace396d5061930e1adcf
gift56 <efeasiughu@gmail.com>
2022-10-28 17:39:21 +0100

    added sorting column

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -74,7 +74,7 @@ const Order = () => {
     }
     setSortingColumn(column);
   }
-  const sortedData = sortBy(data);
+  const sortedData = sortBy(data,sortingColumn);
 
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
