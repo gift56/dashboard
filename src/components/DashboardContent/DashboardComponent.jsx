9180478fd82486020b7f6763c14163886aa8aa2e
gift56 <efeasiughu@gmail.com>
2022-10-28 12:27:18 +0100

    added space between

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -6,7 +6,18 @@ import TopSales from "./TopSales";
 const DashboardComponent = () => {
   return (
     <main className="p-5 w-full bg-white my-2 mr-8 rounded-3xl">
-      <h1 className="text-primary text-xl mb-4">Welcome <b>Efe Gift</b></h1>
+      <div className="mb-4 flex justify-between">
+        <h1 className="text-primary text-xl">
+          Welcome <b>Efe Gift</b>
+        </h1>
+        <div className="w-10">
+          <img
+            src="https://yt3.ggpht.com/fm3vHmjvtRVzSW7bSPmR5IGTrwQ14TEqgyn2LjlXLpdrlnwCW28nPMxORHoJYWUWCreuKZi5-bo=s600-c-k-c0x00ffffff-no-rj-rp-mo"
+            alt="profile_Img"
+            className="w-full rounded-full"
+          />
+        </div>
+      </div>
       <div className="flex gap-5">
         <div>
           <Boxes />
