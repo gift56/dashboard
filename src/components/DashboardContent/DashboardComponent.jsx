fbda9b0038250eb9b02604b7308a5ff80317226c
gift56 <efeasiughu@gmail.com>
2022-10-28 12:36:07 +0100

    imported the components

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Boxes from "./Boxes";
 import ChartBox from "./ChartBox";
 import TopSales from "./TopSales";
+import Welcome from "./Welcome";
 
 const DashboardComponent = () => {
   return (
     <main className="p-5 w-full bg-white my-2 mr-8 rounded-3xl">
-      
+      <Welcome />
       <div className="flex gap-5">
         <div>
           <Boxes />
