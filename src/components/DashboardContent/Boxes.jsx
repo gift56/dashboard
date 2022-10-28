42ee662f0a4ea3a6dae7a7b9312774e4c84b7915
gift56 <efeasiughu@gmail.com>
2022-10-28 10:37:22 +0100

    removed some classes thats not needed

--- src/components/DashboardContent/Boxes.jsx
+++ src/components/DashboardContent/Boxes.jsx
@@ -9,12 +9,12 @@ const Boxes = () => {
         <BsBarChartLine className="w-8 h-8" />
         <div className="">
           <h2 className="text-2xl font-bold leading-4">$345</h2>
-          <h3 className="">Profit today</h3>
+          <h3>Profit today</h3>
         </div>
       </div>
       <div className="bg-red-200  w-1/2 text-primary p-5 rounded-tr-3xl rounded-bl-3xl rounded-tl-md rounded-br-md flex items-center gap-2">
         <AiOutlineInbox className="w-8 h-8" />
-        <div className="">
+        <div>
           <h2 className="text-2xl font-bold leading-4">21 392</h2>
           <h3 className="">Orders this month</h3>
         </div>
