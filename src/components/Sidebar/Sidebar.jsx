e799c037d5a1c30635f2af5df32c857d5cde8cac
gift56 <efeasiughu@gmail.com>
2022-10-28 13:16:16 +0100

    workingn some stuff

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,6 +8,10 @@ import { BsGear, BsShop } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const active =
+    "bg-white text-primary rounded-xl rounded-r-none px-4 py-2 pr-7 flex gap-2 items-center";
+  const inActive = "px-4 py-2 flex gap-2 items-center";
+
   return (
     <aside className="bg-primary text-white p-5 py-10 pr-0">
       <Link to="/" className="flex mb-6 items-center justify-center mr-2 gap-1">
@@ -15,26 +19,23 @@ const Sidebar = () => {
         <span>YourShop</span>
       </Link>
       <nav>
-        <Link
-          to="/"
-          className="bg-white text-primary rounded-xl rounded-r-none px-4 py-2 pr-7 flex gap-2 items-center"
-        >
+        <Link to="/" className={active}>
           <AiOutlineHome />
           <span>Dashboard</span>
         </Link>
-        <Link to="/orders" className="px-4 py-2 flex gap-2 items-center">
+        <Link to="/orders" className={inActive}>
           <AiOutlineUnorderedList />
           <span>Orders</span>
         </Link>
-        <Link to="/products" className="flex gap-2 items-center px-4 py-2">
+        <Link to="/products" className={inActive}>
           <AiOutlineInbox />
           Products
         </Link>
-        <Link to="/reviews" className="flex gap-2 items-center px-4 py-2">
+        <Link to="/reviews" className={inActive}>
           <AiOutlineStar />
           <span>Reviews</span>
         </Link>
-        <Link to="/settings" className="flex gap-2 items-center px-4 py-2">
+        <Link to="/settings" className={inActive}>
           <BsGear />
           <span>Settings</span>
         </Link>
