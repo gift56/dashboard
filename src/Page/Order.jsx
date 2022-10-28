82d9f0371ede5deea3b81f65975841a4ea16fa0c
gift56 <efeasiughu@gmail.com>
2022-10-28 14:33:51 +0100

    working on others

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Order = () => {
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
-      Order
+      <h1>Orders</h1>
     </div>
   );
 };
