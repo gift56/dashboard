f807222f61b41e620ce243a3fdeb8e46348b14f5
gift56 <efeasiughu@gmail.com>
2022-10-28 12:35:25 +0100

    created welcome components

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -6,21 +6,7 @@ import TopSales from "./TopSales";
 const DashboardComponent = () => {
   return (
     <main className="p-5 w-full bg-white my-2 mr-8 rounded-3xl">
-      <div className="mb-4 flex justify-between">
-        <h1 className="text-primary text-xl">
-          Welcome <b>Efe Gift</b>
-        </h1>
-        <div className="flex bg-primary items-center gap-4 w-1/6 rounded-md">
-          <div className="w-10">
-            <img
-              src="https://yt3.ggpht.com/fm3vHmjvtRVzSW7bSPmR5IGTrwQ14TEqgyn2LjlXLpdrlnwCW28nPMxORHoJYWUWCreuKZi5-bo=s600-c-k-c0x00ffffff-no-rj-rp-mo"
-              alt="profile_Img"
-              className="w-full rounded-md"
-            />
-          </div>
-          <h2 className="font-bold text-white leading-4">Efe Gift</h2>
-        </div>
-      </div>
+      
       <div className="flex gap-5">
         <div>
           <Boxes />
