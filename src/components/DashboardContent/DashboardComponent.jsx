6b8449158b7a787d385a7db1dd6ae6f88a4b3dc3
gift56 <efeasiughu@gmail.com>
2022-10-28 11:37:39 +0100

    working on layour

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -5,8 +5,13 @@ import ChartBox from "./ChartBox";
 const DashboardComponent = () => {
   return (
     <main className="p-5 w-[90%] bg-white my-2 mr-8 rounded-3xl">
-      <Boxes />
-      <ChartBox />
+      <div className="flex">
+        <div>
+          <Boxes />
+          <ChartBox />
+        </div>
+        <div></div>
+      </div>
     </main>
   );
 };
