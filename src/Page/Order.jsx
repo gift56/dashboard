222d90abf5ab9c1c512266682a431ae2ae93cf4e
gift56 <efeasiughu@gmail.com>
2022-10-28 14:41:49 +0100

    added normal font

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -3,14 +3,16 @@ import React from "react";
 const Order = () => {
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
-      <h1 className="text-primary font-bold text-2xl sm:text-3xl mb-4">Orders</h1>
-      <table>
+      <h1 className="text-primary font-bold text-2xl sm:text-3xl mb-4">
+        Orders
+      </h1>
+      <table className="w-full">
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Date</th>
-            <th>Address</th>
-            <th>Products</th>
+            <th className="font-normal">ID</th>
+            <th className="font-normal">Date</th>
+            <th className="font-normal">Address</th>
+            <th className="font-normal">Products</th>
           </tr>
         </thead>
       </table>
