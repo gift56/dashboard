2e40475b3b1a2b0670096b8fed5c3211c3d62289
gift56 <efeasiughu@gmail.com>
2022-10-28 09:10:58 +0100

    good morning

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -3,7 +3,7 @@ import { BsBarChartLine } from "react-icons/bs";
 
 const DashboardComponent = () => {
   return (
-    <main className="p-5 grow bg-white my-2 mr-2 rounded-lg">
+    <main className="p-5 grow bg-white my-2 mr-2 rounded-3xl">
       <div className="flex flex-col gap-3">
         <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
           <BsBarChartLine className="w-8 h-8" />
