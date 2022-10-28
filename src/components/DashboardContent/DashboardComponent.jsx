51a8ea72bb3737a0b5792d31aa7361fd30d6022f
gift56 <efeasiughu@gmail.com>
2022-10-28 12:22:02 +0100

    added ing we

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -6,7 +6,7 @@ import TopSales from "./TopSales";
 const DashboardComponent = () => {
   return (
     <main className="p-5 w-full bg-white my-2 mr-8 rounded-3xl">
-      <h1 className="text-primary text-xl mb-4">WelCome</h1>
+      <h1 className="text-primary text-xl mb-4">Welcome <b>Efe Gift</b></h1>
       <div className="flex gap-5">
         <div>
           <Boxes />
