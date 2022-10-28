eb1e77e626d26819696ce94c47654d8b5126094e
gift56 <efeasiughu@gmail.com>
2022-10-28 12:01:50 +0100

    done creating objects

--- src/components/DashboardContent/TopSales.jsx
+++ src/components/DashboardContent/TopSales.jsx
@@ -5,6 +5,14 @@ const TopSales = () => {
     {
       id: 1,
       img: "https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385",
+      name: "Power Minimal Sports Bra",
+      price: "100",
+    },
+    {
+      id: 2,
+      img: "https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385",
+      name: "Power Minimal Sports Bra",
+      price: "100",
     },
   ];
 
@@ -16,26 +24,11 @@ const TopSales = () => {
           <span className="text-gray-500 text-sm">1.</span>
           <img src="" alt="iamges" className="w-12 rounded-full h-12" />
           <div>
-            <span className="text-base block text-primary leading-5">
-              Power Minimal Sports Bra
-            </span>
-            <span className="text-primary font-bold text-xl">$100</span>
+            <span className="text-base block text-primary leading-5"></span>
+            <span className="text-primary font-bold text-xl">$</span>
           </div>
         </div>
-        <div className="flex items-center gap-3">
-          <span className="text-gray-500 text-sm">1.</span>
-          <img
-            src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
-            alt="iamges"
-            className="w-12 rounded-full h-12"
-          />
-          <div>
-            <span className="text-base block text-primary leading-5">
-              Power Minimal Sports Bra
-            </span>
-            <span className="text-primary font-bold text-xl">$100</span>
-          </div>
-        </div>
+       
       </div>
     </>
   );
