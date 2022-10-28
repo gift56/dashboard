eca49a553dd1ea755b6d01b22d24128754ae032e
gift56 <efeasiughu@gmail.com>
2022-10-28 13:53:22 +0100

    working on mobile view

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -9,7 +9,7 @@ const DashboardComponent = () => {
     <main className="p-5 w-full bg-white my-2 mr-8 rounded-3xl">
       <Welcome />
       <div className="flex flex-col sm:flex-row gap-7">
-        <div className="w-3/5 grow">
+        <div className="w-full sm:w-3/5 grow">
           <Boxes />
           <ChartBox />
         </div>
