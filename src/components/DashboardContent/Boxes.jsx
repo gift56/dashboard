4648d03d5888832c1d01e44382924cf041a169ea
gift56 <efeasiughu@gmail.com>
2022-10-28 09:42:53 +0100

    imported boxes components

--- src/components/DashboardContent/Boxes.jsx
+++ src/components/DashboardContent/Boxes.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { BsBarChartLine } from "react-icons/bs";
+import { AiOutlineInbox } from "react-icons/ai";
 
 const Boxes = () => {
   return (
-    <div>Boxes</div>
-  )
-}
+    <div className="flex  gap-3">
+      <div className="bg-blue-200 grow w-1/2 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center gap-2">
+        <BsBarChartLine className="w-8 h-8" />
+        <div className="">
+          <h2 className="text-2xl font-bold leading-4">$345</h2>
+          <h3 className="">Profit today</h3>
+        </div>
+      </div>
+      <div className="bg-red-200 grow w-1/2 text-primary p-5 rounded-tr-3xl rounded-bl-3xl rounded-tl-md rounded-br-md flex items-center gap-2">
+        <AiOutlineInbox className="w-8 h-8" />
+        <div className="">
+          <h2 className="text-2xl font-bold leading-4">21 392</h2>
+          <h3 className="">Orders this month</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Boxes
+export default Boxes;
--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-
+import Boxes from "./Boxes";
 
 const DashboardComponent = () => {
   return (
     <main className="p-5 grow bg-white my-2 mr-2 rounded-3xl">
-      <div className="flex  gap-3">
-        <div className="bg-blue-200 grow w-1/2 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center gap-2">
-          <BsBarChartLine className="w-8 h-8" />
-          <div className="">
-            <h2 className="text-2xl font-bold leading-4">$345</h2>
-            <h3 className="">Profit today</h3>
-          </div>
-        </div>
-        <div className="bg-red-200 grow w-1/2 text-primary p-5 rounded-tr-3xl rounded-bl-3xl rounded-tl-md rounded-br-md flex items-center gap-2">
-          <AiOutlineInbox className="w-8 h-8" />
-          <div className="">
-            <h2 className="text-2xl font-bold leading-4">21 392</h2>
-            <h3 className="">Orders this month</h3>
-          </div>
-        </div>
-      </div>
+      <Boxes />
     </main>
   );
 };
