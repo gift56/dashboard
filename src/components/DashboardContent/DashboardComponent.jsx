cd3e1a54a84ba17d0ce1d2e5d9d79a9d9f0dfafe
gift56 <efeasiughu@gmail.com>
2022-10-28 09:45:39 +0100

    added chartbox components

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Boxes from "./Boxes";
+import ChartBox from "./ChartBox";
 
 const DashboardComponent = () => {
   return (
     <main className="p-5 grow bg-white my-2 mr-2 rounded-3xl">
       <Boxes />
+      <ChartBox />
     </main>
   );
 };
