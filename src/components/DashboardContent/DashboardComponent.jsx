6bc058925d9d4d261fe3b0fbcd5355510093d6cf
gift56 <efeasiughu@gmail.com>
2022-10-28 09:14:35 +0100

    changed the gaps

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -4,7 +4,7 @@ import { BsBarChartLine } from "react-icons/bs";
 const DashboardComponent = () => {
   return (
     <main className="p-5 grow bg-white my-2 mr-2 rounded-3xl">
-      <div className="flex flex-col gap-3">
+      <div className="flex  gap-3">
         <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
           <BsBarChartLine className="w-8 h-8" />
           <div className="">
@@ -13,7 +13,6 @@ const DashboardComponent = () => {
           </div>
         </div>
         <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
-    
           <BsBarChartLine className="w-8 h-8" />
           <div className="">
             <h2 className="text-2xl font-bold leading-4">$345</h2>
