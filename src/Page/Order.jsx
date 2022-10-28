cb31468d3a6716899317e12b1f56eb6c1a9e0b75
gift56 <efeasiughu@gmail.com>
2022-10-28 15:29:21 +0100

    sortedData

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -46,6 +46,8 @@ const Order = () => {
     },
   ];
 
+  const sortedData = data;
+
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
       <h1 className="text-primary font-bold text-2xl sm:text-3xl mb-4">
