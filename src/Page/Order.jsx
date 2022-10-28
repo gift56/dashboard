6d75a30c16b2dca54737f8dcd3e31e3d2d119c48
gift56 <efeasiughu@gmail.com>
2022-10-28 17:40:58 +0100

    added sortDirection

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -74,7 +74,7 @@ const Order = () => {
     }
     setSortingColumn(column);
   }
-  const sortedData = sortBy(data,sortingColumn);
+  const sortedData = sortBy(data, sortingColumn, sortDirection.toLocaleLowerCase());
 
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
