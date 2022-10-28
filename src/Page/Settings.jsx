dbd2c315b9b8a66c2e39e22d1f7b520067a964b6
gift56 <efeasiughu@gmail.com>
2022-10-28 14:16:53 +0100

    changed those stuffs

--- src/Page/Settings.jsx
+++ src/Page/Settings.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Settings = () => {
   return (
-    <div className="p-5 w-full bg-white my-2 mr-8 rounded-3xl">
+    <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
       Settings
     </div>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
