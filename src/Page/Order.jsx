8d669fcf27bb38f085df12b1dd88865353fbd2e1
gift56 <efeasiughu@gmail.com>
2022-10-28 14:35:31 +0100

    added updates

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Order = () => {
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
-      <h1 className="text-primary font-bold text-xl sm:text-3xl">Orders</h1>
+      <h1 className="text-primary font-bold text-2xl sm:text-3xl">Orders</h1>
     </div>
   );
 };
