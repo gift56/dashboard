f056d97830b57901004c78c55217b0e4e8299c08
gift56 <efeasiughu@gmail.com>
2022-10-28 12:34:17 +0100

    adding space

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -10,15 +10,15 @@ const DashboardComponent = () => {
         <h1 className="text-primary text-xl">
           Welcome <b>Efe Gift</b>
         </h1>
-        <div className="flex items-center w-1/6  rounded-md">
+        <div className="flex bg-primary items-center gap-4 w-1/6 rounded-md">
           <div className="w-10">
             <img
               src="https://yt3.ggpht.com/fm3vHmjvtRVzSW7bSPmR5IGTrwQ14TEqgyn2LjlXLpdrlnwCW28nPMxORHoJYWUWCreuKZi5-bo=s600-c-k-c0x00ffffff-no-rj-rp-mo"
               alt="profile_Img"
-              className="w-full rounded-full"
+              className="w-full"
             />
           </div>
-          <h2 className="font-bold text-primary leading-4">Efe Gift</h2>
+          <h2 className="font-bold text-white leading-4">Efe Gift</h2>
         </div>
       </div>
       <div className="flex gap-5">
