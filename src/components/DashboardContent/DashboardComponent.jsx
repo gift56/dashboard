ff6b5b04dc7b1da881590d0d958597c5eb71de07
gift56 <efeasiughu@gmail.com>
2022-10-28 11:56:50 +0100

    addling som spacing

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -12,7 +12,7 @@ const DashboardComponent = () => {
         </div>
         <div className="w-1/4 grow flex flex-col gap-3">
           <h2 className="text-primary font-bold">Top selling products</h2>
-          <div>
+          <div className="flex flex-col gap-4">
             <div className="flex items-center gap-3">
               <span className="text-gray-500 text-sm">1.</span>
               <img
@@ -21,7 +21,23 @@ const DashboardComponent = () => {
                 className="w-12 rounded-full h-12"
               />
               <div>
-                <span className="text-base block text-primary leading-5">Power Minimal Sports Bra</span>
+                <span className="text-base block text-primary leading-5">
+                  Power Minimal Sports Bra
+                </span>
+                <span className="text-primary font-bold text-xl">$100</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <span className="text-gray-500 text-sm">1.</span>
+              <img
+                src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
+                alt="iamges"
+                className="w-12 rounded-full h-12"
+              />
+              <div>
+                <span className="text-base block text-primary leading-5">
+                  Power Minimal Sports Bra
+                </span>
                 <span className="text-primary font-bold text-xl">$100</span>
               </div>
             </div>
