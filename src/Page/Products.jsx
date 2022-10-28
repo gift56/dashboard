974faf650ecc07d995905c6a861f72b2bda3aefa
gift56 <efeasiughu@gmail.com>
2022-10-28 14:16:37 +0100

    working on mobile view

--- src/Page/Products.jsx
+++ src/Page/Products.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Products = () => {
   return (
-    <div className="p-5 w-full bg-white my-2 mr-8 rounded-3xl">Products</div>
+    <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
+      Products
+    </div>
   );
 };
 
