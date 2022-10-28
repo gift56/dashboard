3f29e32c1f3f9aeaa1c216c1915c79fa0a057b74
gift56 <efeasiughu@gmail.com>
2022-10-28 11:38:23 +0100

    working on layout

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -5,12 +5,12 @@ import ChartBox from "./ChartBox";
 const DashboardComponent = () => {
   return (
     <main className="p-5 w-[90%] bg-white my-2 mr-8 rounded-3xl">
-      <div className="flex">
+      <div className="flex gap-5">
         <div>
           <Boxes />
           <ChartBox />
         </div>
-        <div></div>
+        <div>Right</div>
       </div>
     </main>
   );
