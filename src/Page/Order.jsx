be2070287f4ef90347a8fe867509929e8f5710c2
gift56 <efeasiughu@gmail.com>
2022-10-28 14:53:38 +0100

    added padding

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -9,21 +9,23 @@ const Order = () => {
       <table className="w-full">
         <thead>
           <tr>
-            <th className="font-medium">ID</th>
-            <th className="font-medium">Date</th>
-            <th className="font-medium">Address</th>
-            <th className="font-medium">Products</th>
+            <th className="font-medium bg-[#ddd] px-[15px] py-[2px]">ID</th>
+            <th className="font-medium bg-[#ddd] px-[15px] py-[2px]">Date</th>
+            <th className="font-medium bg-[#ddd] px-[15px] py-[2px]">Address</th>
+            <th className="font-medium bg-[#ddd] px-[15px] py-[2px]">Products</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="">1.</td>
-            <td className="">2022-10-12 18:34 CET</td>
-            <td className="">
+            <td className="bg-[#ddd] px-[15px] py-[2px]">1.</td>
+            <td className="bg-[#ddd] px-[15px] py-[2px]">2022-10-12 18:34 CET</td>
+            <td className="bg-[#ddd] px-[15px] py-[2px]">
               Robert Robertson ,<br />
               1234 NW Bobcat Lane
             </td>
-            <td className="">Sport Hoodie, Boxing bag, Ugly hat, Short jean</td>
+            <td className="bg-[#ddd] px-[15px] py-[2px]">
+              Sport Hoodie, Boxing bag, Ugly hat, Short jean
+            </td>
           </tr>
         </tbody>
       </table>
