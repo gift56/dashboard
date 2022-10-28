617ba6c9538d1cc21c18220f7949a710e87cf1a5
gift56 <efeasiughu@gmail.com>
2022-10-28 11:53:59 +0100

    added some stuff

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -21,8 +21,8 @@ const DashboardComponent = () => {
                 className="w-12 rounded-full h-12"
               />
               <div>
-                <span className="text-sm block">Power Minimal Sports Bra</span>
-                <span>$100</span>
+                <span className="text-sm block text-primary">Power Minimal Sports Bra</span>
+                <span className="text-primary font-bold text-lg">$100</span>
               </div>
             </div>
           </div>
