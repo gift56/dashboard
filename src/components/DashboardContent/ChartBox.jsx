207ebeb57a24af978b7172457128b5bf2eb00fd4
gift56 <efeasiughu@gmail.com>
2022-10-28 10:34:36 +0100

    changed some stuff

--- src/components/DashboardContent/ChartBox.jsx
+++ src/components/DashboardContent/ChartBox.jsx
@@ -13,44 +13,44 @@ const ChartBox = () => {
   const data = [
     {
       name: "January",
-      uv: 4000,
-      pv: 2400,
+      sales: 4000,
+      revenue: 2400,
       amt: 2400,
     },
     {
       name: "February",
-      uv: 3000,
-      pv: 1398,
+      sales: 3000,
+      revenue: 1398,
       amt: 2210,
     },
     {
       name: "March",
-      uv: 2000,
-      pv: 9800,
+      sales: 2000,
+      revenue: 9800,
       amt: 2290,
     },
     {
       name: "April",
-      uv: 2780,
-      pv: 3908,
+      sales: 2780,
+      revenue: 3908,
       amt: 2000,
     },
     {
       name: "May",
-      uv: 1890,
-      pv: 4800,
+      sales: 1890,
+      revenue: 4800,
       amt: 2181,
     },
     {
       name: "June",
-      uv: 2390,
-      pv: 3800,
+      sales: 2390,
+      revenue: 3800,
       amt: 2500,
     },
     {
       name: "July",
-      uv: 3490,
-      pv: 4300,
+      sales: 3490,
+      revenue: 4300,
       amt: 2100,
     },
   ];
@@ -84,14 +84,14 @@ const ChartBox = () => {
           <Tooltip />
           <Area
             type="monotone"
-            dataKey="uv"
+            dataKey="revenue"
             stroke="#8884d8"
             fillOpacity={1}
             fill="url(#colorUv)"
           />
           <Area
             type="monotone"
-            dataKey="pv"
+            dataKey="sales"
             stroke="#82ca9d"
             fillOpacity={1}
             fill="url(#colorPv)"
