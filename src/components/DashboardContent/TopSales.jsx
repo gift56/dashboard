69404eb469e93d51535aa7cf4bf926c9b5095ed3
gift56 <efeasiughu@gmail.com>
2022-10-28 12:00:38 +0100

    creating array of objects

--- src/components/DashboardContent/TopSales.jsx
+++ src/components/DashboardContent/TopSales.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const TopSales = () => {
-  const sales = [{
-    
-  }];
+  const sales = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385",
+    },
+  ];
 
   return (
     <>
@@ -11,11 +14,7 @@ const TopSales = () => {
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-3">
           <span className="text-gray-500 text-sm">1.</span>
-          <img
-            src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
-            alt="iamges"
-            className="w-12 rounded-full h-12"
-          />
+          <img src="" alt="iamges" className="w-12 rounded-full h-12" />
           <div>
             <span className="text-base block text-primary leading-5">
               Power Minimal Sports Bra
