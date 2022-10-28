4884d7757812898188c35c93a45b33a2f91c5625
gift56 <efeasiughu@gmail.com>
2022-10-28 11:59:06 +0100

    added thos components

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Boxes from "./Boxes";
 import ChartBox from "./ChartBox";
+import TopSales from "./TopSales";
 
 const DashboardComponent = () => {
   return (
@@ -11,37 +12,7 @@ const DashboardComponent = () => {
           <ChartBox />
         </div>
         <div className="w-1/4 grow flex flex-col gap-3">
-          <h2 className="text-primary font-bold">Top selling products</h2>
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center gap-3">
-              <span className="text-gray-500 text-sm">1.</span>
-              <img
-                src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
-                alt="iamges"
-                className="w-12 rounded-full h-12"
-              />
-              <div>
-                <span className="text-base block text-primary leading-5">
-                  Power Minimal Sports Bra
-                </span>
-                <span className="text-primary font-bold text-xl">$100</span>
-              </div>
-            </div>
-            <div className="flex items-center gap-3">
-              <span className="text-gray-500 text-sm">1.</span>
-              <img
-                src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
-                alt="iamges"
-                className="w-12 rounded-full h-12"
-              />
-              <div>
-                <span className="text-base block text-primary leading-5">
-                  Power Minimal Sports Bra
-                </span>
-                <span className="text-primary font-bold text-xl">$100</span>
-              </div>
-            </div>
-          </div>
+          <TopSales />
         </div>
       </div>
     </main>
--- src/components/DashboardContent/TopSales.jsx
+++ src/components/DashboardContent/TopSales.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const TopSales = () => {
   return (
-    <></>
-  )
-}
+    <>
+      <h2 className="text-primary font-bold">Top selling products</h2>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-3">
+          <span className="text-gray-500 text-sm">1.</span>
+          <img
+            src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
+            alt="iamges"
+            className="w-12 rounded-full h-12"
+          />
+          <div>
+            <span className="text-base block text-primary leading-5">
+              Power Minimal Sports Bra
+            </span>
+            <span className="text-primary font-bold text-xl">$100</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-3">
+          <span className="text-gray-500 text-sm">1.</span>
+          <img
+            src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
+            alt="iamges"
+            className="w-12 rounded-full h-12"
+          />
+          <div>
+            <span className="text-base block text-primary leading-5">
+              Power Minimal Sports Bra
+            </span>
+            <span className="text-primary font-bold text-xl">$100</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default TopSales
+export default TopSales;
