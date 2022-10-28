d12fd76bc8f5bb14971bbebffcda0d8ed34ee7f1
gift56 <efeasiughu@gmail.com>
2022-10-28 13:01:50 +0100

    almost done with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import DashboardComponent from "./components/DashboardContent/DashboardComponent";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Order from "./Page/Order";
+import Products from "./Page/Products";
+import Preview from "./Page/Preview";
+import Settings from "./Page/Settings";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -9,6 +13,10 @@ const App = () => {
       <Sidebar />
       <Routes>
         <Route path="/" element={<DashboardComponent />} />
+        <Route path="/orders" element={<Order />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/reviews" element={<Preview />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </div>
   );
