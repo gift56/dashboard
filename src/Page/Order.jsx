9126b74cd94c561dc1b0a1a28ec04cd7f733aa96
gift56 <efeasiughu@gmail.com>
2022-10-28 15:12:30 +0100

    working on array of table

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 const Order = () => {
-const data=[
-  {
-    
-  }
-]
-
-
+  const data = [
+    {
+      id: 1,
+      date:""
+    },
+  ];
 
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
@@ -17,16 +16,28 @@ const data=[
       <table className="w-full">
         <thead>
           <tr>
-            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">ID</th>
-            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">Date</th>
-            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">Address</th>
-            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">Products</th>
+            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">
+              ID
+            </th>
+            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">
+              Date
+            </th>
+            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">
+              Address
+            </th>
+            <th className="font-bold px-[15px] py-[8px] align-top text-primary uppercase border-b border-red-300">
+              Products
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">1.</td>
-            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">2022-10-12 18:34 CET</td>
+            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">
+              1.
+            </td>
+            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">
+              2022-10-12 18:34 CET
+            </td>
             <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">
               Robert Robertson ,<br />
               1234 NW Bobcat Lane
@@ -36,8 +47,12 @@ const data=[
             </td>
           </tr>
           <tr>
-            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">2.</td>
-            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">2022-10-12 18:34 CET</td>
+            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">
+              2.
+            </td>
+            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">
+              2022-10-12 18:34 CET
+            </td>
             <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400 border-b border-red-300">
               Robert Robertson ,<br />
               1234 NW Bobcat Lane
@@ -47,8 +62,12 @@ const data=[
             </td>
           </tr>
           <tr>
-            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400">3.</td>
-            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400">2022-10-12 18:34 CET</td>
+            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400">
+              3.
+            </td>
+            <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400">
+              2022-10-12 18:34 CET
+            </td>
             <td className="px-[15px] py-[8px] align-top text-sm leading-4 text-gray-400">
               Robert Robertson ,<br />
               1234 NW Bobcat Lane
