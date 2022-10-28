6e47e2edbee066c6320c1e4cf99fa483a9a5f74d
gift56 <efeasiughu@gmail.com>
2022-10-28 11:46:48 +0100

    added gap

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -13,12 +13,12 @@ const DashboardComponent = () => {
         <div className="w-1/4 flex flex-col gap-3">
           <h2 className="text-primary font-bold">Top selling products</h2>
           <div>
-            <div className="flex items-center">
+            <div className="flex items-center gap-3">
               <span>1</span>
               <img
                 src="https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385"
                 alt="iamges"
-                className="w-12"
+                className="w-12 rounded-full h-12"
               />
             </div>
           </div>
