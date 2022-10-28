917436261df1e8535b9ada88acb5bb64c4d6f598
gift56 <efeasiughu@gmail.com>
2022-10-28 14:36:27 +0100

    working on header

--- src/Page/Order.jsx
+++ src/Page/Order.jsx
@@ -4,6 +4,11 @@ const Order = () => {
   return (
     <div className="p-3 sm:p-5 w-full bg-white sm:my-2 sm:mr-8 sm:rounded-3xl">
       <h1 className="text-primary font-bold text-2xl sm:text-3xl mb-4">Orders</h1>
+      <table>
+        <thead>
+          <tr></tr>
+        </thead>
+      </table>
     </div>
   );
 };
