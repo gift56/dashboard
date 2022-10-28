c31606a850225185357211ce8e48fb361cee46d7
gift56 <efeasiughu@gmail.com>
2022-10-28 12:43:15 +0100

    removed gap

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
-    <div className="flex gap-3 min-h-screen">
+    <div className="flex min-h-screen">
       <Sidebar />
       <DashboardComponent />
     </div>
