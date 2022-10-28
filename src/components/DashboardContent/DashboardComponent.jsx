ca837d0f50e16d72f59ca51a6bd29f7f1edf44f8
gift56 <efeasiughu@gmail.com>
2022-10-28 09:30:26 +0100

    changed border

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -12,7 +12,7 @@ const DashboardComponent = () => {
             <h3 className="">Profit today</h3>
           </div>
         </div>
-        <div className="bg-red-200 grow text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
+        <div className="bg-red-200 grow text-primary p-5 rounded-tr-3xl rounded-bl-3xl rounded-tl-md rounded-br-md flex items-center">
           <BsBarChartLine className="w-8 h-8" />
           <div className="">
             <h2 className="text-2xl font-bold leading-4">$345</h2>
