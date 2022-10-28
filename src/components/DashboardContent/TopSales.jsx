6b44237bb768d252f3ff2bf3d3a83cccf30a4762
gift56 <efeasiughu@gmail.com>
2022-10-28 12:06:32 +0100

    changing data

--- src/components/DashboardContent/TopSales.jsx
+++ src/components/DashboardContent/TopSales.jsx
@@ -6,13 +6,13 @@ const TopSales = () => {
       id: 1,
       img: "https://cdn.shopify.com/s/files/1/0156/6146/products/PowerMinimalSportsBraREP-L-A0030MagentaPinkB4A5J-KBH3-0049_800x.jpg?v=1666103385",
       name: "Power Minimal Sports Bra",
-      price: "100",
+      price: "36",
     },
     {
       id: 2,
       img: "https://cdn.shopify.com/s/files/1/0156/6146/products/PowerCropZipHoodieBlackB3A9X-BBBB-0800_800x.jpg?v=1666103379",
       name: "Power Cropped Zip Hoodie",
-      price: "100",
+      price: "56",
     },
     {
       id: 3,
