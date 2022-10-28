2a011fdd1619adb74bc4481cd7826ee86829bdf0
gift56 <efeasiughu@gmail.com>
2022-10-28 12:17:07 +0100

    added 100% width

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -5,7 +5,7 @@ import TopSales from "./TopSales";
 
 const DashboardComponent = () => {
   return (
-    <main className="p-5 w-[90%] bg-white my-2 mr-8 rounded-3xl">
+    <main className="p-5 w-full bg-white my-2 mr-8 rounded-3xl flex flex-col items-center">
       <div className="flex gap-5">
         <div>
           <Boxes />
