f41f1676d19ac2a69d3af1440a4fd6d7615d4fd5
gift56 <efeasiughu@gmail.com>
2022-10-28 09:28:55 +0100

    added grow class

--- src/components/DashboardContent/DashboardComponent.jsx
+++ src/components/DashboardContent/DashboardComponent.jsx
@@ -5,14 +5,14 @@ const DashboardComponent = () => {
   return (
     <main className="p-5 grow bg-white my-2 mr-2 rounded-3xl">
       <div className="flex  gap-3">
-        <div className="bg-blue-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
+        <div className="bg-blue-200 grow text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
           <BsBarChartLine className="w-8 h-8" />
           <div className="">
             <h2 className="text-2xl font-bold leading-4">$345</h2>
             <h3 className="">Profit today</h3>
           </div>
         </div>
-        <div className="bg-red-200 text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
+        <div className="bg-red-200 grow text-primary p-5 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md flex items-center">
           <BsBarChartLine className="w-8 h-8" />
           <div className="">
             <h2 className="text-2xl font-bold leading-4">$345</h2>
